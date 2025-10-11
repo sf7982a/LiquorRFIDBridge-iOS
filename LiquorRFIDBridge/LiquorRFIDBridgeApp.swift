83f4f4a5a120d74c29641fe6c9afc81be87048d7
Sam Fisher <samfisher0808@gmail.com>
2025-10-11 15:19:06 -0400

    Initial Commit

---
+++ LiquorRFIDBridge/LiquorRFIDBridgeApp.swift
@@ -0,0 +1,17 @@
+//
+//  LiquorRFIDBridgeApp.swift
+//  LiquorRFIDBridge
+//
+//  Created by Samuel Fisher on 10/11/25.
+//
+
+import SwiftUI
+
+@main
+struct LiquorRFIDBridgeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ LiquorRFIDBridgeTests/LiquorRFIDBridgeTests.swift
@@ -0,0 +1,17 @@
+//
+//  LiquorRFIDBridgeTests.swift
+//  LiquorRFIDBridgeTests
+//
+//  Created by Samuel Fisher on 10/11/25.
+//
+
+import Testing
+@testable import LiquorRFIDBridge
+
+struct LiquorRFIDBridgeTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
